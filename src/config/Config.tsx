2a9b5c9fbe247bc2d1a9f931b2b1bc09e28db03e
warto nur prasetyo <->
2023-07-26 05:43:04 +0700

    -

--- src/config/Config.tsx
+++ src/config/Config.tsx
@@ -1,6 +1,6 @@
 const Config: any = {
-  BaseUrlDev: "http://117.74.123.165/api/",
-  // BaseUrlDev: "https://rspaw.or.id/api/",
+  // BaseUrlDev: "http://117.74.123.165/api/",
+  BaseUrlDev: "https://rspaw.or.id/api/",
   BaseUrlProd: "https://rspaw.or.id/api/",
   client_key:
     "9d3b5bc28d45a4a1180ab98b23f8685e6e79f250be7b7b1be0feaecb2b06fb57",
